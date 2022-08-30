42930fc76b2cafe443fd09164a73b6af978538bc
Cesare Montresor <cesare.montresor@gmail.com>
2022-08-30 09:40:18 +0200

    Refactor AuthSerive to UserService

--- src/main/java/it/univr/di/testero/api/UserResponder.java
+++ src/main/java/it/univr/di/testero/api/UserResponder.java
@@ -1,24 +1,19 @@
 package it.univr.di.testero.api;
 
 
-import it.univr.di.testero.config.AuthService;
+import it.univr.di.testero.services.UserService;
 import it.univr.di.testero.model.User;
-import it.univr.di.testero.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserResponder {
     @Autowired
-    private AuthService authService;
-
-    @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @SchemaMapping(typeName = "Query", field = "getUser")
     public User getUser() {
-        return authService.userGet();
+        return userService.userGet();
     }
 }
