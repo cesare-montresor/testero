aabaa88debb1002e7a3f9b7f4b881eb44cd46859
Gabriele <roncolatogabriele@gmail.com>
2022-08-30 11:27:39 +0200

    Added "allTests" graphQL query implementation.

--- src/main/java/it/univr/di/testero/services/IStudentService.java
+++ src/main/java/it/univr/di/testero/services/IStudentService.java
@@ -2,9 +2,13 @@ package it.univr.di.testero.services;
 
 import it.univr.di.testero.model.Compilazione;
 import it.univr.di.testero.model.CompilazioneRisposta;
+import it.univr.di.testero.model.Test;
 import it.univr.di.testero.model.User;
 
+import java.util.List;
+
 public interface IStudentService {
+    List<Test> allTests();
     Compilazione takeTest(Long testId, User user);
     CompilazioneRisposta giveAnswer(Long idCompilazione, Long idDomanda, Long idRisposta);
 }
