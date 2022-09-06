4622c987c0d0096a70d3ecbdd352bfc46c4efdf4
michele <mick0974@outlook.it>
2022-09-06 18:58:17 +0200

    Corretto confronto per selezione radio button

--- src/main/resources/frontend/src/components/NavigationButton.js
+++
@@ -1,7 +0,0 @@
- function NavigationButton(props){
-  return(
-    <button name={props.id} id={props.id} className={props.className} onClick={props.onClick}><label htmlFor={props.id}>{props.label}</label></button>
-  );
-}
-
-export {NavigationButton};
